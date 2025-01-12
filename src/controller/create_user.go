0665ae147423a21986de319276cb93b256b71b93
peruccii <peruccii2917@hotmail.com>
2025-01-12 14:31:28 -0300

    create_user logic, using shouldBindJson to reat request and identify errors

--- src/controller/create_user.go
+++ src/controller/create_user.go
@@ -1,5 +1,31 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"net/http"
+	"peruccii/goapi/src/configurations/rest_err"
+	"peruccii/goapi/src/controller/model/request"
 
-func CreateUser(c *gin.Context) {}
+	"github.com/gin-gonic/gin"
+)
+
+func CreateUser(c *gin.Context) {
+    
+    var userRequest request.UserRequest
+
+    // ShouldBindJSON attempt to read the struct, if an error ( like missing fields )
+    // you can handle the erros.
+    if err := c.ShouldBindJSON(&userRequest); err != nil {
+        restErr := rest_err.NewBadRequestError(
+            fmt.Sprintf("there are some incorrect fields, error=%s\n", err.Error()))
+            // err.Error <-- will take the method on rest_err that returns a message error    
+            
+
+        c.JSON(restErr.Code, restErr) // <-- c.JSON requires an code and err obj
+        
+        return // <-- return to not continues the code
+    }
+
+    c.JSON(http.StatusOK, userRequest)
+        
+}
