b98990329042966df078076bcd438fc0be4867e9
peruccii <peruccii2917@hotmail.com>
2025-01-13 15:09:29 -0300

    encrypt password

--- src/model/user_domain.go
+++ src/model/user_domain.go
@@ -1,6 +1,10 @@
 package model
 
-import "peruccii/goapi/src/configurations/rest_err"
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"peruccii/goapi/src/configurations/rest_err"
+)
 
 // begin to lowerCase bc its private
 type UserDomain struct {
@@ -10,9 +14,16 @@ type UserDomain struct {
     Age         string
 }
 
+func (ud *UserDomain) EncryptPassword() {
+    hash := md5.New()
+    defer hash.Reset()
+    hash.Write([]byte(ud.Password))
+    ud.Password = hex.EncodeToString(hash.Sum(nil))
+}
+
 type UserDomainInterface interface {
-    CreateUser(UserDomain)         *rest_err.RestErr // <-- create and may return an error
-    UpdateUser(string, UserDomain) *rest_err.RestErr // <-- pass user id and your content
+    CreateUser()         *rest_err.RestErr // <-- create and may return an error
+    UpdateUser(string) *rest_err.RestErr // <-- pass user id and your content
     FindUser(string)               (*UserDomain, *rest_err.RestErr) // <-- return User or error
     DeleteUser(string)             *rest_err.RestErr
 }
